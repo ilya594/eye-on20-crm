a662702ccd17fc9617bf457dd04ab39348a30ab4
Ilya <cankillah@gmail.com>
2026-08-09 15:37:29 +0300

    try check flow (ui change)

--- packages/twenty-ui/src/theme/constants/AccentDark.ts
+++ packages/twenty-ui/src/theme/constants/AccentDark.ts
@@ -1,23 +1,22 @@
-import * as RadixColors from '@radix-ui/colors';
-import { COLOR_DARK } from '@ui/theme/constants/ColorsDark';
-
 export const ACCENT_DARK = {
-  primary: COLOR_DARK.blue5,
-  secondary: COLOR_DARK.blue5,
-  tertiary: COLOR_DARK.blue3,
-  quaternary: COLOR_DARK.blue2,
-  accent3570: COLOR_DARK.blue8,
-  accent4060: COLOR_DARK.blue8,
-  accent1: RadixColors.indigoDarkP3.indigo1,
-  accent2: RadixColors.indigoDarkP3.indigo2,
-  accent3: RadixColors.indigoDarkP3.indigo3,
-  accent4: RadixColors.indigoDarkP3.indigo4,
-  accent5: RadixColors.indigoDarkP3.indigo5,
-  accent6: RadixColors.indigoDarkP3.indigo6,
-  accent7: RadixColors.indigoDarkP3.indigo7,
-  accent8: RadixColors.indigoDarkP3.indigo8,
-  accent9: RadixColors.indigoDarkP3.indigo9,
-  accent10: RadixColors.indigoDarkP3.indigo10,
-  accent11: RadixColors.indigoDarkP3.indigo11,
-  accent12: RadixColors.indigoDarkP3.indigo12,
+  primary: '#3d7cff',
+  secondary: '#3d7cff',
+  tertiary: '#263b68',
+  quaternary: '#17233d',
+
+  accent3570: '#5b8cff',
+  accent4060: '#5b8cff',
+
+  accent1: '#111a2d',
+  accent2: '#17233d',
+  accent3: '#1d3158',
+  accent4: '#264272',
+  accent5: '#31558f',
+  accent6: '#3d69a8',
+  accent7: '#4b7ac0',
+  accent8: '#5b8cff',
+  accent9: '#3d7cff',
+  accent10: '#6798ff',
+  accent11: '#9bb9ff',
+  accent12: '#dbe7ff',
 };
